db8855b4c7a40b5931d3cc67847d1bbdd98814af
Pervs <winfourteen@gmail.com>
2023-04-15 17:48:08 +0800

    first commit

---
+++ src/Pages/Home.jsx
@@ -0,0 +1,83 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { SiJavascript,SiReact,SiTailwindcss,SiNodedotjs,SiHtml5,SiCss3,SiMongodb,SiExpress } from 'react-icons/si';
+
+const Home = ({home,bgColorr,themeColor}) => {
+  
+  return (
+    <main className='flex flex-1 sm:overflow-auto px-10 sm:py-10 pb-10 z-20' style={{backgroundColor:themeColor}}>
+      <div className='homeContainer md:w-2/3 w-full'>
+        {home.map((item)=>(
+          <Link to={`/${item.id}`} key={item.id} className='rounded-lg min-h-[130px] p-5 flex flex-col justify-evenly mb-10 drop-shadow-lg overflow-hidden' style={{backgroundColor:bgColorr}}>
+          
+            <div className='title text-2xl mb-3'>
+              <h2>{item.title}</h2>
+            </div>
+
+            <div>
+              <p>
+                {(item.description).length <= 100 ? item.description : `${(item.description).slice(0,100)}...`}
+              </p>
+            </div>
+
+            <div className='flex mt-3'>
+              <div>
+                <p className={`mr-3 px-3 rounded-full bg-[${themeColor}]`}>#{item.category}</p>
+              </div>
+
+              <div className='flex'>
+                <p className='mr-3'>{item.date}</p>
+                <p>{item.time}</p>
+              </div>
+            </div>
+
+          </Link>
+        ))}
+        <div className={`w-24 h-0 border-solid border-2 border-[${bgColorr}] my-16`}></div>
+        <footer>
+            <p className='font-semibold'>&copy; 2022 - 2023 Ziza</p>
+        </footer>
+      </div>
+      <div className='rightSideContainer md:flex flex-col pl-10 justify-between hidden'>
+            <div className='technology'>
+              <div className='title'>
+                <h2 className='uppercase font-semibold mb-5'>Tech Stack</h2>
+              </div>
+              <div className='flex flex-wrap gap-3 text-xl'>
+                <SiJavascript/>
+                <SiReact/>
+                <SiTailwindcss/>
+                <SiNodedotjs/>
+                <SiHtml5/>
+                <SiCss3/>
+                <SiMongodb/>
+                <SiExpress/>
+              </div>
+            </div>
+              <div>
+                <div className='title'>
+                  <h2 className='uppercase font-semibold mb-5'>Key Skills</h2>
+                </div>
+                  <ul className='flex flex-col gap-3 cursor-pointer'>
+                    <li>&#x2022; <span className='underline'>Time Management</span></li>
+                    <li>&#x2022; <span className='underline'>Goal Oriented</span></li>
+                    <li>&#x2022; <span className='underline'>Problem Solving</span></li>
+                    <li>&#x2022; <span className='underline'>Adaptability</span></li>
+                    <li>&#x2022; <span className='underline'>Communication</span></li>
+                  </ul>
+              </div>
+                  <div>
+                    <div className='title'>
+                      <h2 className='uppercase font-semibold mb-5'>Categories</h2>
+                    </div>
+                    <div className='flex gap-3 flex-wrap'>
+                      <Link to="/projects" className={`bg-[${bgColorr}] px-3 rounded-full drop-shadow-md`}>#Projects</Link>
+                      <Link to="/achivements" className={`bg-[${bgColorr}] px-3 rounded-full drop-shadow-md`}>#Achievements</Link>
+                    </div>
+                  </div>
+      </div>
+    </main>
+  )
+}
+
+export default Home
---
+++ src/Pages/Nav.jsx
@@ -0,0 +1,102 @@
+import React from 'react';
+import { FiGithub,FiFacebook } from 'react-icons/fi';
+import { TbBrandDiscord } from 'react-icons/tb';
+import { TfiTwitter } from 'react-icons/tfi';
+import { BiMedal } from 'react-icons/bi';
+import { AiOutlineProject, AiOutlineHome,AiOutlineUser,AiOutlineContacts } from 'react-icons/ai';
+import { RiFilePaper2Line } from 'react-icons/ri';
+import { BsLink45Deg, BsMoonStars,BsSun } from 'react-icons/bs';
+import { Link } from 'react-router-dom';
+import './styles/nav.css';
+import { useState } from 'react';
+
+const Nav = ({theme,themeToggler}) => {
+    const [opener, setOpener] = useState("");
+    const [num, setNum] = useState(0);
+
+    let themeColor;
+    let textColor;
+    if(theme){
+        themeColor = "rgb(245,245,250)"
+        textColor = "rgb(82,100,119)";
+      }else{
+        themeColor = "rgb(48,48,48)"
+        textColor = "rgb(195,195,195)";
+      }
+
+    const handleOpen=()=>{
+        setOpener(()=>opener ? "" : "open");
+        setNum(()=> num ? 0 : 290);
+    }
+
+
+  return (
+    <nav className=' relative px-10 pt-10 pb-5 flex flex-col justify-between sm:w-72 w-full'>
+        <div className={`menu sm:pb-0 pb-[${num}px]`}>
+            <div className='imgContainer'>
+                <img src="./src/assets/profile4.png" alt="profile" className='drop-shadow-xl'/>
+            </div>
+            <div className='textContainer mb-5'>
+                <p className='text-md mb-3 font-bold tracking-widest'>ziza</p>
+                <p className=' text-sm'>A curious person who loves to explore the internet and computers.</p>
+            </div>
+            <div className='socialContainer flex gap-5 text-lg'>
+                <a href="https://github.com/zizaaa"><FiGithub/></a>
+                <a href="https://www.facebook.com/jyrnv"><FiFacebook/></a>
+                <a href="#"><TbBrandDiscord/></a>
+                <a href="#"><TfiTwitter/></a>
+            </div>
+        </div>
+        <div className=' sm:flex flex-col hidden gap-5 text-'>
+            <Link to='/' className='flex items-center'><span className='mr-5'><AiOutlineHome /></span> Home</Link>
+            <Link to='/about' className='flex items-center'><span className='mr-5'><AiOutlineUser /></span> About</Link>
+            {/* <Link to='/achivements' className='flex items-center'><span className='mr-5'><BiMedal /></span> Achivements</Link>
+            <Link to='/projects' className='flex items-center'><span className='mr-5'><AiOutlineProject /></span> Projects</Link> */}
+            <Link to='/contact' className='flex items-center'><span className='mr-5'><AiOutlineContacts /></span> Contact</Link>
+            {/* <Link to='/resume' className='flex items-center'><span className='mr-5'><RiFilePaper2Line /></span> Resume</Link> */}
+            <Link to='/links' className='flex items-center'><span  className='mr-5'><BsLink45Deg /></span> Links</Link>
+        </div>
+        <div>
+            <button className='sm:flex items-center hidden' onClick={themeToggler}>
+                {
+                    theme ? (<BsMoonStars/>) : (<BsSun/>)
+                }
+                {
+                    theme ? <p className='ml-5'>Night</p> : <p className='ml-5'>Light</p>
+                }
+            </button>
+        </div>
+        
+        {/* Hamburger Icon */}{/* hamburger menu */}
+        <div className='absolute right-10 sm:hidden'>
+        <button
+            id="menu-btn"
+            className={`${opener} block hamburger md:hidden focus:outline-none`} onClick={handleOpen}>
+            <span className='hamburger-top'></span>
+            <span className='hamburger-middle'></span>
+            <span className='hamburger-bottom'></span>
+        </button>
+        </div>
+        <div className={`flex-col overflow-hidden  text-lg bg-[${themeColor}] drop-shadow-lg mt-6 p-5 gap-3 flex absolute left-10 right-10 top-[300px] sm:hidden`}>
+        <Link to='/' className='flex items-center'><span className='mr-5'><AiOutlineHome /></span> Home</Link>
+            <Link to='/about' className='flex items-center'><span className='mr-5'><AiOutlineUser /></span> About</Link>
+            {/* <Link to='/achivements' className='flex items-center'><span className='mr-5'><BiMedal /></span> Achivements</Link> */}
+            {/* <Link to='/projects' className='flex items-center'><span className='mr-5'><AiOutlineProject /></span> Projects</Link> */}
+            {/* <Link to='/resume' className='flex items-center'><span className='mr-5'><RiFilePaper2Line /></span> Resume</Link> */}
+            <Link to='/links' className='flex items-center'><span  className='mr-5'><BsLink45Deg /></span> Links</Link>
+            <Link to='/contact' className='flex items-center'><span className='mr-5'><AiOutlineContacts /></span> Contact</Link>
+
+            <button className='flex items-center' onClick={themeToggler}>
+                {
+                    theme ? (<BsMoonStars/>) : (<BsSun/>)
+                }
+                {
+                    theme ? <p className='ml-5'>Night</p> : <p className='ml-5'>Light</p>
+                }
+            </button>
+        </div>
+    </nav>
+  )
+}
+
+export default Nav
